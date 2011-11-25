1e71a6c9c45fd6ab953c2276a25028a911e7e8f1
Benny Born <benny@bennyborn.de>
2011-11-25 08:53:04 +0100

    first commit

---
+++ system/modules/pagetaxonomy/Pagetaxonomy.php
@@ -0,0 +1,53 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  numero2 - Agentur für Internetdienstleistungen <www.numero2.de>
+ * @author     Benny Born <benny.born@numero2.de>
+ * @license    LGPL
+ * @filesource
+ */
+ 
+
+class Pagetaxonomy extends Backend {
+
+
+	public function replacePageInsertTags($strTag) {
+
+		$arrTag = trimsplit('::', $strTag);
+		
+		switch( $arrTag[1] ) {
+
+			case 'taxonomyid':
+			
+				global $objPage;
+				
+				if( $objPage->taxonomyid )
+					return $objPage->taxonomyid;
+				
+				break;
+		}
+		
+		return false;
+	}
+}
+
---
+++ system/modules/pagetaxonomy/config/database.sql
@@ -0,0 +1,19 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * Do not import this file manually but use the TYPOlight *
+-- * install tool to create and maintain database tables!   *
+-- *                                                        *
+-- **********************************************************
+
+-- 
+-- Table `tl_page`
+-- 
+
+CREATE TABLE `tl_page` (
+  `taxonomyid` varchar(5) NOT NULL default '',
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+-- --------------------------------------------------------
